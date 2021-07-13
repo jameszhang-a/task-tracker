49ea7b943c7c2dece526132ae6034493a53ccdb5
James Zhang <jameszhanga@gmail.com>
2021-07-13 02:24:07 -0500

    Able to add tasks to UI and server

--- src/App.js
+++ src/App.js
@@ -41,10 +41,21 @@ function App() {
 	};
 
 	// * Add task
-	const addTask = (task) => {
-		const id = Math.floor(Math.randon() * 1000) + 1;
-		const newTask = { id, ...task };
-		setTasks([ ...tasks, newTask ]);
+	const addTask = async (task) => {
+		const res = await fetch(`http://localhost:5000/tasks`, {
+			method  : 'POST',
+			headers : {
+				'Content-type' : 'application/json'
+			},
+			body    : JSON.stringify(task)
+		});
+
+		const data = await res.json();
+
+		setTasks([ ...tasks, data ]);
+		// const id = Math.floor(Math.randon() * 1000) + 1;
+		// const newTask = { id, ...task };
+		// setTasks([ ...tasks, newTask ]);
 	};
 
 	return (
