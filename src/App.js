d7a2bef2583ff7e6fb800e16dac0291b0b909e0a
James Zhang <jameszhanga@gmail.com>
2021-07-12 21:37:47 -0500

    Same thing with class based component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from './components/Header';
+import React from 'react';
 
-function App() {
-	const name = 'James';
-	const hello = true;
+// function App() {
+// 	const name = 'James';
+// 	const hello = true;
 
-	return (
-		<div className="container">
-			<Header />
-		</div>
-	);
+// 	return (
+// 		<div className="container">
+// 			<Header />
+// 		</div>
+// 	);
+// }
+
+class App extends React.Component {
+	render() {
+		return <h1>Hello from React</h1>;
+	}
 }
 
 export default App;
